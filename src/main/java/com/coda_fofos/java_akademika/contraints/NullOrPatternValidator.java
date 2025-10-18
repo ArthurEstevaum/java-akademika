43b5fd3656cd48f1bcea94b0e8d6710601a0ec27
PetersonNave <petersonmelo.nave@gmail.com>
2025-10-17 21:36:08 -0300

    Add: Constraints

---
+++ src/main/java/com/coda_fofos/java_akademika/contraints/NullOrPatternValidator.java
@@ -0,0 +1,18 @@
+package com.coda_fofos.java_akademika.constraints;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class NullOrPatternValidator implements ConstraintValidator<NullOrPattern, String> {
+    private String pattern;
+
+    @Override
+    public void initialize(NullOrPattern constraintAnnotation) {
+        this.pattern = constraintAnnotation.pattern();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return value == null || value.matches(pattern);
+    }
+}
